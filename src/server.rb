8469d3b3f07b664720b5fce97fc4bbb491465c2b
James Vodanovich <james@jvodan.com>
2015-08-05 14:01:32 +1000

    relative requires

--- src/server.rb
+++ src/server.rb
@@ -1,6 +1,6 @@
 
-require "NetworkListener.rb"
-require "ProtocolListener.rb"
+require_relative "NetworkListener.rb"
+require_relative "ProtocolListener.rb"
 
 protocol_listener = ProtocolListener.new()
 network_listener = NetworkListener.new(protocol_listener,"127.0.0.1",21027)
