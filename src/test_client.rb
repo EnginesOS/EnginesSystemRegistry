e68fa73ff49b0a7b85dafe8553443c3dd9d84472
James Vodanovich <james@jvodan.com>
2015-08-05 19:00:25 +1000

    test tcp

--- src/test_client.rb
+++ src/test_client.rb
@@ -3,19 +3,43 @@ require 'json'
 
 
 def wait_for_reply
+  def process_messages(socket)
+      
+        # while socket.is_open? ==true 
+      #blocking read
+      #readup to first ,
+      #get count
+      #sub traact bytes already read and read until the rest.
+      #save next segment if there is any (or stay sync)
+        bytes = socket.gets
+        mesg_lng_str = bytes.substring(0,bytes.indexof(','))
+        mesg_len =  Integer.parse(mesg_lng_str)
+        message_response = bytes.substring(bytes.indexof(','))
+        
+        while message_response.size < mesg_len
+         more = socket.gets
+          message_response = message_response + more
+        end 
+       
+
+       end
   
-  response , address = @registry_socket.recvfrom(32762)
-  p "Got reply from " + address.to_s
+       p :got 
+       p message_response
  
-  return response
+  return message_response
   
 end
-
+def build_mesg(mesg_str)
+     header = mesg_str.to_s.length
+     return header.to_s + "," + mesg_str.to_s
+   end
 def send_request(command,params)
   request_hash = params.dup
   request_hash[:command] = command
    request_json = request_hash.to_json
-  @registry_socket.send(request_json,0,"127.0.0.1",21027)
+  mesg_str = build_mesg(request_json)
+  @registry_socket.puts(mesg_str)
   wait_for_reply
 end
 
@@ -24,14 +48,13 @@ end
 def open_socket(host,port)
    require 'socket.rb' 
   BasicSocket.do_not_reverse_lookup = true
-    socket = UDPSocket.new(Socket::AF_INET)
-    if socket     
-      socket.bind(host,port)           
+    socket = TCPSocket.new(host,port)  
+    
       return socket
-    end
+
 end   
 
-@registry_socket= open_socket("127.0.0.1",21028)
+@registry_socket= open_socket("127.0.0.1",21027)
 
 params=Hash.new
 
