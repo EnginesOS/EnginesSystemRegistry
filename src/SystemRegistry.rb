d9c1a6031e74db6df0f342298febaa95ce3ab2d6
James Vodanovich <james@jvodan.com>
2015-08-05 15:46:13 +1000

    require ruby tree

--- src/SystemRegistry.rb
+++ src/SystemRegistry.rb
@@ -7,6 +7,8 @@ class SystemRegistry < Registry
   require_relative 'ServicesRegistry.rb'
   require_relative 'OrphanServicesRegistry.rb'
   require_relative 'SystemUtils.rb'
+  require 'rubytree'
+  
    
   #@ call initialise Service Registry Tree which loads it from disk or create a new one if none exits
    def initialize() 
