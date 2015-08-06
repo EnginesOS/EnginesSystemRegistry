783af436726a9de1734df0b1cd8b0e4fa3525854
James Vodanovich <james@jvodan.com>
2015-08-06 18:59:14 +1000

    catch more errors with bigger block

--- src/ProtocolListener.rb
+++ src/ProtocolListener.rb
@@ -39,13 +39,13 @@ class ProtocolListener
 #       p :request_hash
 #       p request_hash
     
-       
+    begin 
        method_symbol = command.to_sym
        request_method = @system_registry.method(method_symbol)
        method_params = request_method.parameters
        p method_params
        p "invoking " + command.to_s
-       begin
+    
        if method_params.length ==0
          response_object =  @system_registry.public_send(method_symbol,nil)
        else
