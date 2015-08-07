86def3cf448f600f3e7728f80ac908b7f283c720
James Vodanovich <james@jvodan.com>
2015-08-07 13:35:14 +1000

    send instead of write

--- src/NetworkListener.rb
+++ src/NetworkListener.rb
@@ -105,7 +105,7 @@ end
 
     begin
      
-     socket.write(reply)
+     socket.send(reply)
      # socket.recv(0) #check it's open anc hcuck wobbly if not
     rescue  IO::EAGAINWaitWritable
       retry_count+=1
