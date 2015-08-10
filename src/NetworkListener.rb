4d89e61080e3d793e59412efb98f4cb1813f2e20
James Vodanovich <james@jvodan.com>
2015-08-10 17:07:37 +1000

    formatication

--- src/NetworkListener.rb
+++ src/NetworkListener.rb
@@ -1,41 +1,41 @@
 require 'yaml'
 require 'timeout'
 require 'thread'
+
 class NetworkListener
-  
   def initialize(protocol_listener,ip,socket)
     @registry_listener = start_network_server(ip,socket)
     @protocol_listener = protocol_listener
     @registry_lock  = Mutex.new()
   end
-  
+
   #Fix me need to limit connections close thread etc
   def listen_for_messages
     loop do
-      client = @registry_listener.accept       
+      client = @registry_listener.accept
       log_connection(client)
-       if  check_request_source_address(client) == true
-            thr = Thread.new {   process_messages(client) }
-       end
+      if  check_request_source_address(client) == true
+        thr = Thread.new {   process_messages(client) }
+      end
     end
   end
-  
+
   def log_connection(client)
-           client_ipdetails = client.peeraddr(true,:numeric)
-           p "Connection on " + client_ipdetails[2].to_s + ":" + client_ipdetails[1].to_s
-   end
-   
+    client_ipdetails = client.peeraddr(true,:numeric)
+    p "Connection on " + client_ipdetails[2].to_s + ":" + client_ipdetails[1].to_s
+  end
+
   def send_error(socket,request_hash,result)
     request_hash[:result] = "Error"
     request_hash[:error] = result
-    send_result(socket,request_hash) 
+    send_result(socket,request_hash)
   end
-  
-  def send_ok_result(socket,result)    
+
+  def send_ok_result(socket,result)
     result[:result] = "OK"
-    send_result(socket,result)      
+    send_result(socket,result)
   end
-  
+
   def process_first_chunk(mesg_data)
 
     total_length = mesg_data.size
@@ -47,172 +47,171 @@ class NetworkListener
 
     return message_request , mesg_len
   end
-  
+
   def process_messages(socket)
-    
+
     session_timer_thread=1
-    while true 
-      begin   
-          message_request= String.new
-          first_bytes=true
-          
+    while true
+      begin
+        message_request= String.new
+        first_bytes=true
+
         mesg_len = 1 #will set on first pass
-      while message_request.size < mesg_len
-        begin
-          #p :getting_mesg_data
-          mesg_data = socket.read_nonblock(1500)
-          p mesg_data
-        if first_bytes == true
-         # session_timer_thread = Thread.new {sleep 5}
-          first_bytes = false
-          message_request, mesg_len= process_first_chunk(mesg_data)
-#          message_request = deheaded_chunk[0]
-#           mesg_len   = deheaded_chunk[1]       
-        else
-          message_request = message_request + mesg_data
-#           if session_timer_thread.is_running == false
-#             p :Timeout
-#           end
-        end
-        
-       if message_request.size >= mesg_len
-        
-          break
-       end
-       
-        rescue IO::EAGAINWaitReadable
-          if message_request.size >= mesg_len
-             break
-          end
-                retry
+        while message_request.size < mesg_len
+          begin
+            #p :getting_mesg_data
+            mesg_data = socket.read_nonblock(1500)
+            p mesg_data
+            if first_bytes == true
+              # session_timer_thread = Thread.new {sleep 5}
+              first_bytes = false
+              message_request, mesg_len= process_first_chunk(mesg_data)
+              #          message_request = deheaded_chunk[0]
+              #           mesg_len   = deheaded_chunk[1]
+            else
+              message_request = message_request + mesg_data
+              #           if session_timer_thread.is_running == false
+              #             p :Timeout
+              #           end
+            end
+
+            if message_request.size >= mesg_len
+
+              break
+            end
+
+          rescue IO::EAGAINWaitReadable
+            if message_request.size >= mesg_len
+              break
+            end
+            retry
           rescue Errno::EIO
             p :EIO
-          return  
-         rescue Errno::ECONNRESET
             return
-         rescue Errno::EPIPE
-           p :EPIPE
-            return 
-         rescue EOFError
-          #End of Message
-                return  
-      rescue Exception=>e
-        p e.to_s
-        p e.backtrace.to_s
+          rescue Errno::ECONNRESET
+            return
+          rescue Errno::EPIPE
+            p :EPIPE
+            return
+          rescue EOFError
+            #End of Message
+            return
+          rescue Exception=>e
+            p e.to_s
+            p e.backtrace.to_s
+          end
         end
-      end 
 
-      request_hash = convert_request_to_hash(message_request)
+        request_hash = convert_request_to_hash(message_request)
 
-      @registry_lock.synchronize   {
-    result = @protocol_listener.perform_request(request_hash)                 
-                 if result  != false
-                   send_ok_result(socket,result)
-                 else                
-                   send_error(socket,request_hash,result)
-                 end
-      }      
+        @registry_lock.synchronize   {
+          result = @protocol_listener.perform_request(request_hash)
+          if result  != false
+            send_ok_result(socket,result)
+          else
+            send_error(socket,request_hash,result)
+          end
+        }
+      end
     end
   end
-end
-  
+
   def send_result(socket,reply_hash)
     retry_count=0
     reply_yaml=reply_hash.to_yaml
     reply = build_mesg(reply_yaml)
-   begin
+    begin
       status = Timeout::timeout(15) {
         bytes =  socket.send(reply,0)
-        p "registry_reply " + bytes.to_s + " bytes" 
+        p "registry_reply " + bytes.to_s + " bytes"
       }
-     
-     # socket.recv(0) #check it's open anc hcuck wobbly if not
+
+      # socket.recv(0) #check it's open anc hcuck wobbly if not
     rescue  IO::EAGAINWaitWritable
       retry_count+=1
       retry
-      rescue  Timeout::Error 
-         @last_error="Timeout sending reply"
-         return false
+    rescue  Timeout::Error
+      @last_error="Timeout sending reply"
+      return false
     rescue Exception=>e
       p  e.to_s
       p e.backtrace.to_s
       return false
+    end
+
+    return true
   end
-     
-    return true  
-  end
-  
+
   def build_mesg(mesg_str)
     header = mesg_str.to_s.length
     return header.to_s + "," + mesg_str.to_s
   end
-  
+
   def check_request_source_address(client)
     ip = client.peeraddr(true,:numeric)
     #Stub for ip ACL rules
     return true
   end
-  
+
   def check_request(request_str,source_address)
-    if check_request_source_address(source_address) == false     
+    if check_request_source_address(source_address) == false
       return false
     end
-   return true
+    return true
   end
-  
+
   def convert_request_to_hash(request)
-   hash_request = YAML::load(request)
+    hash_request = YAML::load(request)
     return hash_request # symbolize_top_level_keys(hash_request)
 
   end
- 
+
   def symbolize_top_level_keys(hash)
-      hash.inject({}){|result, (key, value)|
-        new_key = case key
-        when String then key.to_sym
-          else key
-        end
-        result[new_key] = value
-        result
-      }
-    end
-  
+    hash.inject({}){|result, (key, value)|
+      new_key = case key
+      when String then key.to_sym
+      else key
+      end
+      result[new_key] = value
+      result
+    }
+  end
+
   def symbolize_keys(hash)
-      hash.inject({}){|result, (key, value)|
-        new_key = case key
-        when String then key.to_sym
-        else key
-        end
-        new_value = case value
-        when Hash then symbolize_keys(value)
-        when Array   then
-          newval=Array.new
-          value.each do |array_val|
-            if array_val.is_a?(Hash)
-              array_val = symbolize_keys(array_val)
-            end
-            newval.push(array_val)
+    hash.inject({}){|result, (key, value)|
+      new_key = case key
+      when String then key.to_sym
+      else key
+      end
+      new_value = case value
+      when Hash then symbolize_keys(value)
+      when Array   then
+        newval=Array.new
+        value.each do |array_val|
+          if array_val.is_a?(Hash)
+            array_val = symbolize_keys(array_val)
           end
-          newval
-        else value
+          newval.push(array_val)
         end
-        result[new_key] = new_value
-        result
-      }
-    end
-    
- def shutdown
-    
- 
-   
- end
+        newval
+      else value
+      end
+      result[new_key] = new_value
+      result
+    }
+  end
+
+  def shutdown
+
+  end
   protected
+
   def start_network_server(host,port)
-    
+
     require 'socket'
     BasicSocket.do_not_reverse_lookup = true
     server = TCPServer.new(host,port)
-    return server   
+    return server
   end
-  
+
 end
--- src/ProtocolListener.rb
+++ src/ProtocolListener.rb
@@ -1,71 +1,68 @@
 class ProtocolListener
   attr_accessor :last_error
-  
-  require_relative 'system_registry/SystemRegistry.rb'
-  
 
+  require_relative 'system_registry/SystemRegistry.rb'
   def initialize()
     @system_registry = SystemRegistry.new
 
   end
-  
+
   def  perform_request(command_hash)
     if is_command_hash_valid?(command_hash)== false
       return false
     end
-   
+
     command = command_hash[:command]
     request = command_hash[:value]
-        
+
     response_hash = Hash.new
     response_hash[:command]=command
     response_hash[:request_value]=command_hash[:value]
     response_object=""
     begin
-  
-       method_symbol = command.to_sym
-       request_method = @system_registry.method(method_symbol)
-       method_params = request_method.parameters
-       p method_params
-       p "invoking " + command.to_s + " with " + method_params.to_s
-    
-       if method_params.length ==0
-         response_object =  @system_registry.public_send(method_symbol)
-       else
-         response_object = @system_registry.public_send(method_symbol,request)
-       end  
-      
-       rescue Exception=>e
-         p e.to_s
-         p "with " + request.to_s + " " +  command.to_s + e.backtrace.to_s
- 
-         return false
-       end
- 
-  
+
+      method_symbol = command.to_sym
+      request_method = @system_registry.method(method_symbol)
+      method_params = request_method.parameters
+      p method_params
+      p "invoking " + command.to_s + " with " + method_params.to_s
+
+      if method_params.length ==0
+        response_object =  @system_registry.public_send(method_symbol)
+      else
+        response_object = @system_registry.public_send(method_symbol,request)
+      end
+
+    rescue Exception=>e
+      p e.to_s
+      p "with " + request.to_s + " " +  command.to_s + e.backtrace.to_s
+
+      return false
+    end
+
     if response_object.is_a?(Tree::TreeNode)
       response_object = response_object.detached_subtree_copy
     end
-    
+
     response_hash[:object] = response_object.to_yaml
- 
-   return response_hash
+
+    return response_hash
 
   end
-  
+
   def is_command_hash_valid?(command_hash)
     if command_hash == nil
-          return false
-    elsif command_hash.has_key?(:command) == false 
-          @last_error="Error_non_command"
-          return false
+      return false
+    elsif command_hash.has_key?(:command) == false
+      @last_error="Error_non_command"
+      return false
     elsif command_hash[:command]  == nil
       @last_error = "nil command"
-       return false
-     end
-     return true
+      return false
+    end
+    return true
   end
-  
+
   def shutdown
   end
 
