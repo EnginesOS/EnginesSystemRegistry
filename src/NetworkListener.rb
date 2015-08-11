72e5d84c9b638c115367780f2983be4aaf66f691
James Vodanovich <james@jvodan.com>
2015-08-11 17:59:21 +1000

    handle packet split in middle of read

--- src/NetworkListener.rb
+++ src/NetworkListener.rb
@@ -87,6 +87,7 @@ class NetworkListener
               if message_request.size >   mesg_len
                 p :read_complete_NEED_TO_UNGET
                 socket.ungetbyte(message_request[mesg_len,message_request.size])
+              message_request = message_request[0,mesg_len]
               p message_request.size.to_s + " of " + mesg_len.to_s
               end
               break
