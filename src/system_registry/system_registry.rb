460e1eeaa128c85d308a913cfdd91d246bed75bc
James L Vodanovich <james@jvodan.com>
2015-10-25 06:41:11 +0000

    log failure to gan lock

--- src/system_registry/system_registry.rb
+++ src/system_registry/system_registry.rb
@@ -401,9 +401,11 @@ class SystemRegistry < Registry
     if File.exist?(@@RegistryLock)
       sleep 1
       sleep 1 if File.exist?(@@RegistryLock)
-      return false if File.exist?(@@RegistryLock)
+      p :REGISTRY_LOCKED
+      return log_error_mesg("Failed to lock",@@RegistryLock) if File.exist?(@@RegistryLock)
     end
     FileUtils.touch(@@RegistryLock)
+    return true
   end
   
   def unlock_tree
