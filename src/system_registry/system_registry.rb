cc5c27b64dc039091a3b38fa9a96f4986a14412c
James L Vodanovich <james@jvodan.com>
2015-10-26 03:25:46 +0000

    load tree each time TESTING ONLY

--- src/system_registry/system_registry.rb
+++ src/system_registry/system_registry.rb
@@ -145,6 +145,7 @@ class SystemRegistry < Registry
     clear_error
     service_tree_file = '/opt/engines/run/service_manager/services.yaml'   
       current_mod_time = File.mtime(service_tree_file)
+    @last_tree_mod_time = nil
       if @last_tree_mod_time.nil? || !@last_tree_mod_time.eql?(current_mod_time)
         @system_registry = load_tree
         set_registries
