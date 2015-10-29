566c80916e98c174f33f8d7504aa9978a3777a90
James L Vodanovich <james@jvodan.com>
2015-10-29 02:28:20 +0000

    require rubytree

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -1,9 +1,11 @@
 
   require 'rest-client'
+  require 'rubytree'
   r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', nil)
  p :get_tree_test
  obj = JSON.parse(r)
  p obj.to_s
+ 
  
 params = {}
 params[:name]='cert_auth'
