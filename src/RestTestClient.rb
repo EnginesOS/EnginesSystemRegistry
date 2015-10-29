542eb7f7b14f93c3abc72120ceb8711566144ae7
James L Vodanovich <james@jvodan.com>
2015-10-29 02:00:53 +0000

    correct verb

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -1,4 +1,4 @@
 
   require 'rest-client'
-  r =   RestClient.post('http://127.0.0.1:4567/system_registry/configurations_tree', TrueClass.to_json, :content_type => :json, :accept => :json)
+  r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', TrueClass.to_json, :content_type => :json, :accept => :json)
  p r
