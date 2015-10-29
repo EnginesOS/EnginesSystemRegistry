9b0c84c3c682e2cb06fa8026556b00da2b795ae3
James L Vodanovich <james@jvodan.com>
2015-10-29 02:02:53 +0000

    args for get

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -1,4 +1,4 @@
 
   require 'rest-client'
-  r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', TrueClass.to_json,  :accept => :json)
+  r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', TrueClass.to_json)
  p r
