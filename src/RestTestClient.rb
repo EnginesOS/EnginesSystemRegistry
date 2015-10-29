8f8cf6a0a34edc016d9312575a7d872c714f87da
James L Vodanovich <james@jvodan.com>
2015-10-29 03:27:57 +0000

    service_name key not name

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -13,14 +13,14 @@ p :get_tree_test
  
 p :get_hashes_test
 params = {}
-params[:name]='cert_auth'
+params[:service_name]='cert_auth'
 r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations/',{:params => params })
  obj = JSON.parse(r, :create_additions => true)
  p obj.to_s
  
 p :get_hash_test
 params = {}
-params[:name]='cert_auth'
+params[:service_name]='cert_auth'
 params[:configurator_name]='system_ca'
 r =   RestClient.get('http://127.0.0.1:4567/system_registry/configuration/',{:params => params })
 
