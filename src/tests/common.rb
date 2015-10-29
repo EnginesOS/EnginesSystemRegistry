87675d4218914375a05175325f58485caee93a6b
James L Vodanovich <james@jvodan.com>
2015-10-29 10:56:52 +0000

    retun false if http code >399

--- src/tests/common.rb
+++ src/tests/common.rb
@@ -20,6 +20,7 @@ def parse_rest_response(r)
   return nil if r.nil? || r == ''
    return false if r == 'false'
    return true if r == 'true'
+    return false if r.code > 399
    res = JSON.parse(r, :create_additions => true)
    return symbolize_keys(res) if res.is_a?(Hash)
    return res 
@@ -28,22 +29,24 @@ def parse_rest_response(r)
      return false
 end
 
-
+def base_url
+  'http://127.0.0.1:4567'
+end
 
 require 'rest-client'
 
 def rest_get(path,params)
-  parse_rest_response(RestClient.get('http://127.0.0.1:4567' + path, params)) 
+  parse_rest_response(RestClient.get(base_url + path, params)) 
 end
 
 def rest_post(path,params)
-  parse_rest_response(RestClient.post('http://127.0.0.1:4567' + path, params))
+  parse_rest_response(RestClient.post(base_url + path, params))
 end
 
 def rest_put(path,params)
-  parse_rest_response(RestClient.put('http://127.0.0.1:4567' + path, params))
+  parse_rest_response(RestClient.put(base_url + path, params))
 end
 
 def rest_delete(path,params)
-  parse_rest_response(RestClient.delete('http://127.0.0.1:4567' + path, params))
+  parse_rest_response(RestClient.delete(base_url + path, params))
 end
