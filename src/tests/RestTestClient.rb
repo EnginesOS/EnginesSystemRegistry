a2d6d8dda878e66608d9cff34f38ffdf11119926
liJames L Vodanovich <james@jvodan.com>
2015-10-29 11:01:39 +0000

    ll

--- src/tests/RestTestClient.rb
+++ src/tests/RestTestClient.rb
@@ -6,6 +6,6 @@ require_relative '../utils.rb'
 require_relative 'common.rb'
 require_relative 'configuration_tests.rb'
 require_relative 'managed_engines_tests.rb'
-require_relative 'managed_services_tests.rb'
-require_relative 'orphan_services_tests.rb'
+#require_relative 'managed_services_tests.rb'
+#require_relative 'orphan_services_tests.rb'
 
