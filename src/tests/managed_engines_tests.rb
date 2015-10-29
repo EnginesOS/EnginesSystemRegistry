6e0dc24f1702674ac152dfa30c9d234b9b153692
James L Vodanovich <james@jvodan.com>
2015-10-29 08:25:18 +0000

    more tests

--- src/tests/managed_engines_tests.rb
+++ src/tests/managed_engines_tests.rb
@@ -71,5 +71,50 @@ params[:service_container_name] = 'dns'
 obj = rest_get('/system_registry/engine_service_hash/',{:params => params })
 test_failed('Failed to find added', obj) unless obj.is_a?(Hash)
 
+annouce_test("Update service hash from an engine")
+
+params = {}
+params[:container_type] = 'service'
+params[:publisher_namespace] = 'EnginesSystem'
+params[:type_path] = 'dns'
+params[:parent_engine] = 'test'
+params[:service_handle] = 'test_dns'
+params[:service_container_name] = 'dns'
+params[:variables] = {}
+params[:variables][:service_handle] = params[:service_handle]
+params[:variables][:ip] = 'ip2'
+params[:variables][:hostname] = 'ip2'
+obj = rest_get('/system_registry/engine_service_hash/',{:params => params })
+test_failed('Failed to update', obj) unless obj == true
+params = {}
+params[:container_type] = 'service'
+params[:publisher_namespace] = 'EnginesSystem'
+params[:type_path] = 'dns'
+params[:parent_engine] = 'test'
+params[:service_handle] = 'test_dns'
+params[:service_container_name] = 'dns'
+obj = rest_get('/system_registry/engine_service_hash/',{:params => params })
+test_failed('Failed to find modified', obj) unless obj.is_a?(Hash)
+test_failed(' modified Failed', obj) unless obj[:variables][:hostname] == 'ip2'
+
+annouce_test("Remove service hash from an engine")
+params = {}
+params[:container_type] = 'service'
+params[:publisher_namespace] = 'EnginesSystem'
+params[:type_path] = 'dns'
+params[:parent_engine] = 'test'
+params[:service_handle] = 'test_dns'
+params[:service_container_name] = 'dns'
+obj = rest_delete('/system_registry/remove_from_managed_engines_registry/',{:params => params })
+test_failed('Failed to del added', obj) unless obj == true
+params = {}
+params[:container_type] = 'service'
+params[:publisher_namespace] = 'EnginesSystem'
+params[:type_path] = 'dns'
+params[:parent_engine] = 'test'
+params[:service_handle] = 'test_dns'
+params[:service_container_name] = 'dns'
+obj = rest_get('/system_registry/engine_service_hash/',{:params => params })
+test_failed('Failed as found deleted ', obj) if obj != false
 
 
