256994d2affa21b2bbd8c3c9edcbdabedb3e6259
James L Vodanovich <james@jvodan.com>
2015-10-29 09:25:31 +0000

    missed path

--- src/tests/managed_services_tests.rb
+++ src/tests/managed_services_tests.rb
@@ -3,6 +3,6 @@
 test_type('Managed Services Regsitry')
 
 annouce_test("Tree")
-obj = rest_get('/system_registry/services/tree/', nil)
+obj = rest_get('/system_registry/services/tree', nil)
 test_failed('Loading Tree', obj) unless obj.is_a?(Tree::TreeNode)
 
