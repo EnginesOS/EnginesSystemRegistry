9cb58d90053f787ca1ad2e05199cb920200a9779
James L Vodanovich <james@jvodan.com>
2015-10-29 03:49:22 +0000

    test add

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -14,7 +14,7 @@ p :get_tree_test
 p :get_hashes_test
 params = {}
 params[:service_name]='cert_auth'
-r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations/',{:params => params })
+r = RestClient.get('http://127.0.0.1:4567/system_registry/configurations/',{:params => params })
  obj = JSON.parse(r, :create_additions => true)
  p obj.to_s
  
@@ -27,4 +27,14 @@ r =   RestClient.get('http://127.0.0.1:4567/system_registry/configuration/',{:pa
 
  obj = JSON.parse(r, :create_additions => true)
  p obj.to_s
+p :get_hash_test
+params = {}
+params[:service_name]='cert_auth'
+params[:configurator_name]='test_ca'
+config_hash[:variables] = {}
+    config_hash[:variables][:test_var] = "TEST INGS"
+r = RestClient.post('http://127.0.0.1:4567/system_registry/configuration/',{:params => params }.to_json, :content_type => :json, :accept => :json)
+obj = JSON.parse(r, :create_additions => true)
+ p obj.to_s
+ 
  
