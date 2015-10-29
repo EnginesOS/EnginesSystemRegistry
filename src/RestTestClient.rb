0252d80c25cb1fa207fb1b1bb8adb3b9892df32a
James L Vodanovich <james@jvodan.com>
2015-10-29 02:16:20 +0000

    further test

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -1,6 +1,15 @@
 
   require 'rest-client'
   r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', nil)
- p r
+ p :get_tree_test
  obj = JSON.parse(r)
  p obj.to_s
+ 
+params = {}
+params[:name]='cert_auth'
+r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations',params)
+
+p :get_hashs_test
+ obj = JSON.parse(r)
+ p obj.to_s
+ 
