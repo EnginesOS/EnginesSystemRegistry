0654f07fd40064e23f080fcaaab6c98a84c7bb87
James L Vodanovich <james@jvodan.com>
2015-10-29 02:50:07 +0000

    send as json

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -1,29 +1,30 @@
 
   require 'rest-client'
   require 'rubytree'
+  
+p :get_tree_test
   r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', nil)
- p :get_tree_test
+
  obj = JSON.parse(r, :create_additions => true)
  p r.class
  p 'PPPPPPPPPPPPPPPPP'
  p obj.class
  p 'OOOOOOOOOOOOOOOOO'
  
- 
+p :get_hashes_test
 params = {}
 params[:name]='cert_auth'
-r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations',params)
-
-p :get_hashes_test
+r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations',params.to_json)
  obj = JSON.parse(r, :create_additions => true)
  p obj.to_s
  
+p :get_hash_test
 params = {}
 params[:name]='cert_auth'
 params[:configurator_name]='system_ca'
 r =   RestClient.get('http://127.0.0.1:4567/system_registry/configuration',params)
 
-p :get_hash_test
+
  obj = JSON.parse(r, :create_additions => true)
  p obj.to_s
  
