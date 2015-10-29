cf78dfb047f3bcc4f1322197c9d308e9df00b754
James L Vodanovich <james@jvodan.com>
2015-10-29 09:27:01 +0000

    correct new path

--- src/tests/orphan_services_tests.rb
+++ src/tests/orphan_services_tests.rb
@@ -1,4 +1,4 @@
 test_type('Orphaned Services Regsitry')
 annouce_test("Tree")
-obj = rest_get('/system_registry/orphan_services_tree', nil)
+obj = rest_get('/system_registry/services/orphans/tree', nil)
 test_failed('Loading Tree', obj) unless obj.is_a?(Tree::TreeNode)
