bdd7eada4e202f45d798576aff0431427369061f
James L Vodanovich <james@jvodan.com>
2015-10-29 02:03:45 +0000

    no args in tree

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -1,4 +1,4 @@
 
   require 'rest-client'
-  r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', TrueClass.to_json)
+  r =   RestClient.get('http://127.0.0.1:4567/system_registry/configurations_tree', nil)
  p r
