ec4bdc8726d4f4aeb21324ad5884db1d8d083497
James L Vodanovich <james@jvodan.com>
2015-10-29 06:12:49 +0000

    mbt

--- src/RestTestClient.rb
+++ src/RestTestClient.rb
@@ -89,9 +89,6 @@ obj = JSON.parse(r, :create_additions => true)
 p :MANAGED_SERVCES_tree_error unless obj.is_a?(Tree::TreeNode)
 
 
-
-
-
 p :ORPHAN_SERVICES
 r =   RestClient.get('http://127.0.0.1:4567/system_registry/orphan_services_tree', nil)
 obj = JSON.parse(r, :create_additions => true)
