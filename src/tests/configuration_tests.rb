f0b0d2cd2286fd3b65b84e17211d3cd48ab93ea1
James L Vodanovich <james@jvodan.com>
2015-10-29 11:25:13 +0000

    llte

--- src/tests/configuration_tests.rb
+++ src/tests/configuration_tests.rb
@@ -53,10 +53,7 @@ params[:configurator_name]='test_ca'
 obj = rest_delete('/system_registry/services/configurations/',{:params => params } )
 test_failed('Delete a Configuration for a configurator', obj) unless obj == true
 annouce_test("Check Deleted Hash")
-
 obj = rest_get('/system_registry/services/configuration/',{:params => params })
-
 test_failed('Delete (acutally) a Configuration for a configurator', obj)  unless obj.is_a?(Hash)
-p :l
 
  
