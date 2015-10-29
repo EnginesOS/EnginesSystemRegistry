bb6a00a77967e4e0714dafad45bd7178a5bf6621
James L Vodanovich <james@jvodan.com>
2015-10-29 04:17:56 +0000

    break out configurations

--- src/RestServer.rb
+++ src/RestServer.rb
@@ -7,61 +7,7 @@ require 'rubytree'
 
    @@system_registry = SystemRegistry.new
     
-  get '/system_registry/configurations_tree' do
-
-   @@system_registry.service_configurations_registry_tree.to_json
-  end
-
-  get '/system_registry/configurations/' do
-  
-    p :request_query_string
-    p request.query_string
-    p :params
-   
-    r_params = symbolize_keys(params)
-    p r_params
-    @@system_registry.get_service_configurations_hashes(symbolize_keys(params)).to_json
-  end
-
-  get '/system_registry/configuration/' do
-   
-    p request.query_string
-    p params
-   @@system_registry.get_service_configuration(symbolize_keys(params)).to_json
-  end
-  
-  post '/system_registry/configuration/' do
-
-    :post_new_conf
-    p request.query_string
-       p params
-     if@@system_registry.add_service_configuration(symbolize_keys(params))
-       status(202)
-     else
-       status(404)
-     end    
-  end
-  
-  put '/system_registry/configuration/' do
- 
-  if@@system_registry.update_service_configuration(symbolize_keys(params))
-    status(202)
-  else
-    status(404)
-  end    
-end
-
-delete '/system_registry/configuration/' do
-
-  p request.query_string
-      p params
-  if@@system_registry.rm_service_configuration(symbolize_keys(params))
-     status(202)
-   else
-     status(404)
-   end    
- end
- 
+ require_relative 'configurations.rb'
  
  
 def symbolize_keys(hash)
---
+++ src/configurations.rb
@@ -0,0 +1,36 @@
+get '/system_registry/configurations_tree' do
+
+  @@system_registry.service_configurations_registry_tree.to_json
+ end
+
+ get '/system_registry/configurations/' do
+   @@system_registry.get_service_configurations_hashes(symbolize_keys(params)).to_json
+ end
+
+ get '/system_registry/configuration/' do
+  @@system_registry.get_service_configuration(symbolize_keys(params)).to_json
+ end
+ 
+ post '/system_registry/configuration/' do
+    if@@system_registry.add_service_configuration(symbolize_keys(params))
+      status(202)
+    else
+      status(404)
+    end    
+ end
+ 
+ put '/system_registry/configuration/' do 
+ if@@system_registry.update_service_configuration(symbolize_keys(params))
+   status(202)
+ else
+   status(404)
+ end    
+end
+
+delete '/system_registry/configuration/' do
+ if@@system_registry.rm_service_configuration(symbolize_keys(params))
+    status(202)
+  else
+    status(404)
+  end    
+end
