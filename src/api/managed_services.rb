c5f288891793961a82926c3f5599561e52273ed5
James L Vodanovich <james@jvodan.com>
2015-10-31 09:26:33 +0000

    fic route

--- src/api/managed_services.rb
+++ src/api/managed_services.rb
@@ -1,34 +1,34 @@
 require_relative 'utils.rb'
 
-get '/system_registry/services/tree' do
+get '/v0/system_registry/services/tree' do
   @@system_registry.services_registry_tree.to_json
 end
  
-get '/system_registry/service/registered/engines/' do
+get '/v0/system_registry/service/registered/engines/' do
   @@system_registry.all_engines_registered_to(RegistryUtils.symbolize_keys(params)).to_json
 end
 
-get '/system_registry/service/consumers/' do
+get '/v0/system_registry/service/consumers/' do
   @@system_registry.find_service_consumers(RegistryUtils.symbolize_keys(params)).to_json
 end
 
-get '/system_registry/service/registered/' do
+get '/v0/system_registry/service/registered/' do
   @@system_registry.get_registered_against_service(RegistryUtils.symbolize_keys(params)).to_json
 end
 
-get '/system_registry/services/providers/in_use/' do
+get '/v0/system_registry/services/providers/in_use/' do
   @@system_registry.list_providers_in_use().to_json
 end
 
-get '/system_registry/service/' do
+get '/v0/system_registry/service/' do
   @@system_registry.get_service_entry(RegistryUtils.symbolize_keys(params)).to_json
 end
 
-get '/system_registry/service/is_registered' do
+get '/v0/system_registry/service/is_registered' do
   @@system_registry.service_is_registered?(RegistryUtils.symbolize_keys(params)).to_json
 end
 
-post '/system_registry/services/' do
+post '/v0/system_registry/services/' do
   p RegistryUtils.symbolize_keys(params)
  if @@system_registry.add_to_services_registry(RegistryUtils.symbolize_keys(params)).to_json
   status(202)
@@ -37,7 +37,7 @@ else
 end    
 end
 
-put '/system_registry/service/update' do
+put '/v0/system_registry/service/update' do
 if @@system_registry.update_attached_service(RegistryUtils.symbolize_keys(params)).to_json
  status(202)
 else
@@ -45,7 +45,7 @@ else
 end    
 end
 
-delete '/system_registry/service/del' do
+delete '/v0/system_registry/service/del' do
 if @@system_registry.remove_from_services_registry(RegistryUtils.symbolize_keys(params)).to_json
  status(202)
 else
