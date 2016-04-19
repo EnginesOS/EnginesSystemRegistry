2fcafd5059dbc9ddb7cdcdce13ba79a1540f6b0e
James L Vodanovich <james@jvodan.com>
2016-04-19 21:04:41 +1000

    try

--- src/api/shares.rb
+++ src/api/shares.rb
@@ -8,7 +8,7 @@ end
  
       
 post '/v0/system_registry/shares/add' do
-  p RegistryUtils.symbolize_keys(params)
+  
  if @@system_registry.add_to_shares_registry(RegistryUtils.symbolize_keys(params)).to_json
   status(202)
 else
