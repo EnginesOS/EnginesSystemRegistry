c72c58b7467b12eb4ac72751f9e9ce2041d957ea
James L Vodanovich <james@jvodan.com>
2016-05-02 11:32:45 +1000

    error propergate

--- src/registry/registry.rb
+++ src/registry/registry.rb
@@ -138,14 +138,12 @@ class Registry
 
   def log_error_mesg(msg, *objects)
     obj_str = object.to_s.slice(0, 256)
-    @last_error = msg + ':' + obj_str
-    SystemUtils.log_error_mesg(msg, object)
+    @last_error = msg + ':' + obj_str  
     return EnginesRegistryError.new(msg, :error, *objects)
   end
 
   def log_exception(e, *objs)
-    @last_error = e.to_s.slice(0, 256)
-    SystemUtils.log_exception(e)
+    @last_error = e.to_s.slice(0, 256)   
     return EnginesRegistryError.new(e.to_s, :exception, *objs)
   end
 end
