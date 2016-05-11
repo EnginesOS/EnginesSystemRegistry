9b510e2931d9fc451083e0a031f9eb19b10ff184
James L Vodanovich <james@jvodan.com>
2016-05-11 13:29:39 +1000

    errors out

--- src/registry/registry.rb
+++ src/registry/registry.rb
@@ -145,6 +145,7 @@ class Registry
   def log_error_mesg(msg, *objects)
     obj_str = objects.to_s.slice(0, 256)
     @last_error = msg + ':' + obj_str  
+    p @last_error
     return EnginesRegistryError.new(msg, :error, *objects)
   end
   
@@ -156,6 +157,7 @@ def log_warning_mesg(msg, *objects)
  
   def log_exception(e, *objs)
     @last_error = e.to_s.slice(0, 256)   
+    p @last_error
     return EnginesRegistryError.new(e.to_s, :exception, *objs)
   end
 end
