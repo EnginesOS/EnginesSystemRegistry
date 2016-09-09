6a8e0499eea76970b9caf8ae93c3c4d43b0b654f
James L Vodanovich <james@jvodan.com>
2016-09-09 10:43:36 +1000

    fix registry error string

--- src/errors/engines_error.rb
+++ src/errors/engines_error.rb
@@ -11,17 +11,28 @@ class EnginesError # < FalseClass
     @source[2] = caller[4].to_s if caller.count >= 5
 
   end
+def to_h
   
- 
-  def to_json(opt)
-STDERR.puts @error_type.to_s 
-    STDERR.puts  @error_mesg.to_s
-    STDERR.puts @sub_system.to_s 
-    STDERR.puts @source.to_s
-#FixMe this is a kludge
-    STDERR.puts '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_json + '}'
-   j = '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_json + '}'
-   j
+   self.instance_variables.each_with_object({}) { |var, hash| hash[var.to_s.delete("@")] = self.instance_variable_get(var) }
+    
 end
+
+def to_s
+  @sub_system.to_s + ':' +  @error_type.to_s + ':'  + @error_mesg.to_s + ':' + @source.to_s
+end
+  def to_json(opt=nil)
+  return self.to_h.to_json(opt)
+end
+# 
+#  def to_json(opt)
+#STDERR.puts @error_type.to_s 
+#    STDERR.puts  @error_mesg.to_s
+#    STDERR.puts @sub_system.to_s 
+#    STDERR.puts @source.to_s
+##FixMe this is a kludge
+#    STDERR.puts '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_json + '}'
+#   j = '{"error_type":"' + @error_type.to_s + '","error_mesg":"' + @error_mesg.to_s + '","sub_system":"' + @sub_system.to_s + '","source":' + @source.to_json + '}'
+#   j
+#end
 end
 
