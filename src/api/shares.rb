6e3cb60c721d0a49490f951ed86e911fd78b50d3
James L Vodanovich <james@jvodan.com>
2016-11-15 12:12:29 +1100

    post not delete

--- src/api/shares.rb
+++ src/api/shares.rb
@@ -8,7 +8,7 @@ post '/v0/system_registry/shares/add' do
   process_result(system_registry.add_to_shares_registry(RegistryUtils.symbolize_keys(params)))
 end
 
-delete '/v0/system_registry/shares/del' do
+post '/v0/system_registry/shares/del' do
   process_result( system_registry.remove_from_shares_registry(RegistryUtils.symbolize_keys(params)))
 end
 
