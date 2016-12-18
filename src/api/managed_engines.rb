39a21a66474f437c25dd2012ab50eacf0383d56f
James L Vodanovich <james@jvodan.com>
2016-12-19 10:01:45 +1100

    more dbg

--- src/api/managed_engines.rb
+++ src/api/managed_engines.rb
@@ -22,7 +22,7 @@ get '/v0/system_registry/engine/services/persistent/' do
 end
 
 post '/v0/system_registry/engine/services/add' do
-  STDERR.puts( ' ADD to managed engines ' + RegistryUtils.symbolize_keys(params).to_s)
+  STDERR.puts( ' ADD to managed engines ' + params.to_s + ' parsed as ' + RegistryUtils.symbolize_keys(params).to_s)
   process_result(system_registry.add_to_managed_engines_registry(RegistryUtils.symbolize_keys(params)))
 end
 
