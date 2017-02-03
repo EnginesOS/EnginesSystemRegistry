40013fb2d1189dcf0927c61719bdf14bac33f116
James L Vodanovich <james@jvodan.com>
2017-02-03 19:59:05 +1100

    dbg

--- src/api/managed_engines.rb
+++ src/api/managed_engines.rb
@@ -23,7 +23,7 @@ end
 
 post '/v0/system_registry/engine/services/add' do
   p_params = post_params(request)
- # STDERR.puts( ' ADD to managed engines ' + params.to_s + ' parsed as ' +  p_params.to_s)
+  STDERR.puts( ' ADD to managed engines ' + params.to_s + ' parsed as ' +  p_params.to_s)
   process_result(system_registry.add_to_managed_engines_registry(p_params))
 end
 
