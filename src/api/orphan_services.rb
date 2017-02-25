00d3b198c93286b982c84eaa6bc0c71c93e76334
James L Vodanovich <james@jvodan.com>
2017-02-26 09:58:11 +1100

    include vars

--- src/api/orphan_services.rb
+++ src/api/orphan_services.rb
@@ -8,7 +8,7 @@ post '/v0/system_registry/services/orphans/add/:parent_engine/:service_handle/:p
   p_params = post_params(request)
   params.merge(p_params)
 STDERR.puts('New ORPHAN ' +  p_params.to_s + ' _ ' + params.to_s)
-  cparams =  RegistryUtils::Params.assemble_params(cparams, [:parent_engine,:service_handle,:publisher_namespace,:type_path])
+  cparams =  RegistryUtils::Params.assemble_params(cparams, [:parent_engine,:service_handle,:publisher_namespace,:type_path],  :all)
   #cparams.merge(params)
 STDERR.puts('New ORPHAN ' +  cparams.to_s)
   process_result(system_registry.orphanate_service(cparams ))
