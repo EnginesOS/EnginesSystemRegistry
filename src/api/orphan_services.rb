b679110d860061060a4603474dfac88952a81c24
James L Vodanovich <james@jvodan.com>
2017-02-26 09:53:38 +1100

    f

--- src/api/orphan_services.rb
+++ src/api/orphan_services.rb
@@ -8,7 +8,7 @@ post '/v0/system_registry/services/orphans/add/:parent_engine/:service_handle/:p
   p_params = post_params(request)
   params.merge(p_params)
 STDERR.puts('New ORPHAN ' +  p_params.to_s + ' _ ' + params.to_s)
-  cparams =  RegistryUtils::Params.assemble_params(params, [:parent_engine,:service_handle,:publisher_namespace,:type_path])
+  cparams =  RegistryUtils::Params.assemble_params(cparams, [:parent_engine,:service_handle,:publisher_namespace,:type_path])
   #cparams.merge(params)
 
   process_result(system_registry.orphanate_service(cparams ))
@@ -18,9 +18,9 @@ post '/v0/system_registry/services/orphans/return/:parent_engine/:service_handle
   p_params = post_params(request)
   p_params = params['splat']
   p_params.merge(params)
-  params[:type_path] =   splats[0]
+  p_params[:type_path] =   splats[0]
   cparams =  RegistryUtils::Params.assemble_params(p_params, [:parent_engine,:service_handle,:publisher_namespace,:type_path],  :all)
-  process_result(system_registry.rollback_orphaned_service(RegistryUtils.symbolize_keys(params)))
+  process_result(system_registry.rollback_orphaned_service(RegistryUtils.symbolize_keys(cparams)))
 
 end
 
