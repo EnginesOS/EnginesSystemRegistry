c8d2d7e6ed19173063c2c5b87593958d0039070f
James L Vodanovich <james@jvodan.com>
2017-03-02 09:23:30 +1100

    no type path or ns in configurator

--- src/api/configurations.rb
+++ src/api/configurations.rb
@@ -15,14 +15,13 @@ get '/v0/system_registry/services/configuration/:service_name/:configurator_name
   process_result(system_registry.get_service_configuration(cparams))
 end
 
-post '/v0/system_registry/services/configurations/add/:service_name/:configurator_name/:publisher_namespace/*' do
+post '/v0/system_registry/services/configurations/add/:service_name/:configurator_name' do
   #  STDERR.puts( ' ADD to configurations ' + params.to_s + ' parsed as ' +  p_params.to_s)
-  splats = params['splat']
-   params[:type_path] =   splats[0] 
+
   p_params = post_params(request)
   params.merge!(p_params)
 
-  cparams =  RegistryUtils::Params.assemble_params(params, [:service_name,:configurator_name,:publisher_namespace,:type_path],  :all,:all)
+  cparams =  RegistryUtils::Params.assemble_params(params, [:service_name,:configurator_name],  :all,:all)
 cparams[:no_save] = false unless cparams.key(:no_save)
  # STDERR.puts( ' add to configuration ' + cparams.to_s )
 #  p_params = post_params(request)
@@ -30,15 +29,14 @@ cparams[:no_save] = false unless cparams.key(:no_save)
   process_result(system_registry.add_service_configuration(cparams))
 end
 
-post '/v0/system_registry/services/configuration/update/:service_name/:configurator_name/:publisher_namespace/*' do
+post '/v0/system_registry/services/configuration/update/:service_name/:configurator_name' do
   #  STDERR.puts( ' update to configuration ' + params.to_s )
 #  p_params = post_params(request)
 #  p_params.merge(params)
-  splats = params['splat']
-   params[:type_path] =   splats[0] 
+ 
   p_params = post_params(request)
    params.merge!(p_params)
-   cparams =  RegistryUtils::Params.assemble_params(params, [:service_name,:configurator_name,:publisher_namespace,:type_path],  :all,:all)
+   cparams =  RegistryUtils::Params.assemble_params(params, [:service_name,:configurator_name],  :all,:all)
  # STDERR.puts( ' update to configuration ' + cparams.to_s + ':' + params.to_s)
   process_result(system_registry.update_service_configuration(cparams))
 end
