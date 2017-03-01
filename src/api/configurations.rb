708d336f78fa473a0a153924a279cc564ac45a48
James L Vodanovich <james@jvodan.com>
2017-03-01 14:31:31 +1100

    test

--- src/api/configurations.rb
+++ src/api/configurations.rb
@@ -34,7 +34,7 @@ post '/v0/system_registry/services/configuration/update/:service_name/:configura
   p_params = post_params(request)
    params.merge!(p_params)
    cparams =  RegistryUtils::Params.assemble_params(params, [:service_name,:configurator_name],  :all,:all)
-  STDERR.puts( ' update to configuration ' + cparams.to_s )
+  STDERR.puts( ' update to configuration ' + cparams.to_s + ':' + params.to_s)
   process_result(system_registry.update_service_configuration(cparams))
 end
 
