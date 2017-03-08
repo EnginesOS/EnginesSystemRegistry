420756191e45f316eba03cdb25135b95614b084f
James L Vodanovich <james@jvodan.com>
2017-03-08 17:47:49 +1100

    tidy

--- src/utils/params.rb
+++ src/utils/params.rb
@@ -1,9 +1,6 @@
-
-
 module Params
   def self.assemble_params(params, address_params, required_params=nil, accept_params=nil )
     return  nil if params.nil?
-    #params = RegistryUtils.symbolize_keys(params)
     a_params = self.address_params(params, address_params)
     return EnginesError.new('Missing Address Parameters ' + address_params.to_s + ' but only have:' + params.to_s, :error,'api') if a_params == false
 
@@ -16,9 +13,7 @@ module Params
       return EnginesError.new('Missing Parameters ' + required_params.to_s + ' but only have:' + params.to_s, :error,'api') if r_params == false
       a_params.merge!(r_params) unless r_params.nil?
     end
-
     return a_params if accept_params.nil?
-
     unless accept_params.empty?
       o_params = self.optional_params(params,accept_params)
       a_params.merge!(o_params) unless o_params.nil?
@@ -30,39 +25,30 @@ module Params
     mparams = params[:api_vars]
     return false if mparams.nil?
     self.match_params(mparams, keys, true)
-    #   Utils.symbolize_keys(matched)
   end
 
   def self.optional_params(params, keys)
     mparams = params[:api_vars]
-    #  m_params = Utils.symbolize_keys(mparams)
-
-    return nil if mparams.nil?
+    mparams = params if mparams.nil?
     self.match_params(mparams, keys )
-    #   Utils.symbolize_keys(matched)
   end
 
   def self.address_params(params, keys)
-
     self.match_params(params, keys, true)
   end
 
   def self.match_params(params, keys, required = false)
     return  params if keys == :all
-
-    
     return nil if keys.nil?
-    
     cparams =  {}
-    
-      if keys.is_a?(Array)
+    if keys.is_a?(Array)
       for key in keys
         # return missing_param key unless param.key?(key)
         return false  unless self.check_required(params, key,required )
         cparams[key.to_sym] = params[key] unless params[key].nil?
       end
     else
-      return false  unless  self.check_required(params, keys,required)
+      return false unless self.check_required(params, keys,required)
       cparams[keys.to_sym] = params[keys]
     end
     cparams
