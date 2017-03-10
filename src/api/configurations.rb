37dc2883cdda9638d10d8c46d38a6e10c983bd70
James L Vodanovich <james@jvodan.com>
2017-03-10 12:51:35 +1100

    tidy

--- src/api/configurations.rb
+++ src/api/configurations.rb
@@ -1,8 +1,6 @@
-
-
 get '/v0/system_registry/services/configurations/tree' do
   # process_result(RegistryUtils.as_hash(system_registry.service_configurations_registry_tree))
- process_result(registry_as_hash(system_registry.service_configurations_registry_tree))
+  process_result(registry_as_hash(system_registry.service_configurations_registry_tree))
 end
 
 get '/v0/system_registry/service/configurations/:service_name' do
@@ -12,27 +10,26 @@ get '/v0/system_registry/service/configurations/:service_name' do
 end
 
 get '/v0/system_registry/services/configuration/:service_name/:configurator_name' do
-    cparams =  assemble_params(params, [:configurator_name,:service_name], :all,nil)
+  cparams =  assemble_params(params, [:configurator_name,:service_name], :all,nil)
   process_result(system_registry.get_service_configuration(cparams))
 end
 
 post '/v0/system_registry/services/configurations/add/:service_name/:configurator_name' do
   #  STDERR.puts( ' ADD to configurations ' + params.to_s + ' parsed as ' +  p_params.to_s)
-
   params.merge!(post_params(request))
   cparams = assemble_params(params, [:service_name,:configurator_name],  :all,:all)
-cparams[:no_save] = false unless cparams.key?(:no_save)
- # STDERR.puts( ' add to configuration ' + cparams.to_s )
-#  p_params = post_params(request)
-#  p_params.merge(params)
+  cparams[:no_save] = false unless cparams.key?(:no_save)
+  # STDERR.puts( ' add to configuration ' + cparams.to_s )
+  #  p_params = post_params(request)
+  #  p_params.merge(params)
   process_result(system_registry.add_service_configuration(cparams))
 end
 
 post '/v0/system_registry/services/configuration/update/:service_name/:configurator_name' do
   #  STDERR.puts( ' update to configuration ' + params.to_s )
-   params.merge!(post_params(request))
-   cparams =  assemble_params(params, [:service_name,:configurator_name],  :all,:all)
- # STDERR.puts( ' update to configuration ' + cparams.to_s + ':' + params.to_s)
+  params.merge!(post_params(request))
+  cparams =  assemble_params(params, [:service_name,:configurator_name],  :all,:all)
+  # STDERR.puts( ' update to configuration ' + cparams.to_s + ':' + params.to_s)
   process_result(system_registry.update_service_configuration(cparams))
 end
 
