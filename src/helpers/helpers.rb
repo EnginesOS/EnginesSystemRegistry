c6ccc3a1002bbd4e8f870106a2a138ba8d40f95f
James L Vodanovich <james@jvodan.com>
2017-03-10 16:49:09 +1100

    params

--- src/helpers/helpers.rb
+++ src/helpers/helpers.rb
@@ -4,25 +4,25 @@ helpers do
         RegistryUtils.as_hash(tree)
        end
   
-  def assemble_params(params, address_params, required_params=nil, accept_params=nil )
-    STDERR.puts( 'assemble_params Address params ' + params.to_s + ' address keys required ' + address_params.to_s)
-     return  nil if params.nil?
-     params = RegistryUtils.symbolize_keys(params)
-     a_params = address_params(params, address_params)
-     return EnginesError.new('Missing Address Parameters ' + address_params.to_s + ' but only have:' + params.to_s, :error,'api') if a_params == false
+  def assemble_params(ps, address_params, required_params=nil, accept_params=nil )
+    STDERR.puts( 'assemble_params Address params ' + ps.to_s + ' address keys required ' + address_params.to_s)
+     return nil if ps.nil?
+    ps = RegistryUtils.symbolize_keys(ps)
+     a_params = address_params(ps, address_params)
+     return EnginesError.new('Missing Address Parameters ' + address_params.to_s + ' but only have:' + ps.to_s, :error,'api') if a_params == false
  
      unless  required_params.nil? || required_params.empty?
        if required_params == :all
-         a_params.merge!(params[:api_vars]) if params.key?(:api_vars)
+         a_params.merge!(ps[:api_vars]) if ps.key?(:api_vars)
          return a_params
        end
-       r_params = self.required_params(params,required_params)
-       return EnginesError.new('Missing Parameters ' + required_params.to_s + ' but only have:' + params.to_s, :error,'api') if r_params == false
+       r_params = self.required_params(ps,required_params)
+       return EnginesError.new('Missing Parameters ' + required_params.to_s + ' but only have:' + ps.to_s, :error,'api') if r_params == false
        a_params.merge!(r_params) unless r_params.nil?
      end
      return a_params if accept_params.nil?
      unless accept_params.empty?
-       o_params = optional_params(params,accept_params)
+       o_params = optional_params(ps ,accept_params)
        a_params.merge!(o_params) unless o_params.nil?
      end
      a_params
