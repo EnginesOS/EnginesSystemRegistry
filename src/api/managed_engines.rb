d935dd81738a8ea561687572ead42f9f7e6bad48
James L Vodanovich <james@jvodan.com>
2017-03-10 16:53:58 +1100

    f

--- src/api/managed_engines.rb
+++ src/api/managed_engines.rb
@@ -40,7 +40,7 @@ end
 get '/v0/system_registry/engine/services/persistent/:container_type/:parent_engine' do
   STDERR.puts( ' PERS ' + params.to_s )
   cparams =  assemble_params(params, [:parent_engine, :container_type], nil, :all)
-  STDERR.puts( ' PERS ' + cparams.to_s )
+  STDERR.puts( ' PERS ' + params.to_s )
   process_result(system_registry.get_engine_persistent_services(cparams))
 end
 
