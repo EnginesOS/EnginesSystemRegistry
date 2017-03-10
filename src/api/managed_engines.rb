2da498c7dd7aa29816e625893e2323a47fa8bfa9
James L Vodanovich <james@jvodan.com>
2017-03-10 17:02:00 +1100

    f

--- src/api/managed_engines.rb
+++ src/api/managed_engines.rb
@@ -33,7 +33,7 @@ end
 
 get '/v0/system_registry/engine/services/nonpersistent/:container_type/:parent_engine' do
   # STDERR.puts( ' NON PERS ' + RegistryUtils.symbolize_keys(params).to_s)
-  params =  assemble_params(params, [:parent_engine, :container_type], nil, :all)
+  params =  assemble_params(params, [:parent_engine, :container_type])
   process_result(system_registry.get_engine_nonpersistent_services(params))
 end
 
