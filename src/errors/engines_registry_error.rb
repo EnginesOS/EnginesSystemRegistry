cbc91f1a792e45de5f109e16022ededbc32b512e
James L Vodanovich <james@jvodan.com>
2017-03-14 14:23:28 +1100

    feh

--- src/errors/engines_registry_error.rb
+++ src/errors/engines_registry_error.rb
@@ -17,31 +17,31 @@ class EnginesRegistryError < EnginesError
     return EnginesRegistryError.new(e.to_s, e.level, e.params)   
   end
   
-  def log_error_mesg(mesg, *objects)
+  def log_error_mesg(mesg, *objs)
     obj_str = objects.to_s.slice(0, 512)
     @last_error = mesg + ':' + obj_str
     STDERR.puts @last_error.to_s
-    EnginesRegistryError.new(mesg, :error, *objects)
+    EnginesRegistryError.new(mesg, :error, *objs)
   end
 
-  def log_warning_mesg(mesg, *objects)
+  def log_warning_mesg(mesg, *objs)
     obj_str = objects.to_s.slice(0, 256)
     @last_error = mesg + ':' + obj_str
-    EnginesRegistryError.new(mesg, :warning, *objects)
+    EnginesRegistryError.new(mesg, :warning, *objs)
   end
 
   def log_exception(e, *objs)       
     @last_error  = e.to_s
     @last_error  += e.backtrace.to_s
     @last_error = e.to_s.slice(0, 512)
-    EnginesRegistryError.new(@last_error, :exception, *objects)
+    EnginesRegistryError.new(@last_error, :exception, *objs)
   end
 
-  def engines_error(mesg, *objects)
+  def engines_error(mesg, *objs)
     EnginesRegistryError.new(mesg, :error, *objs)
   end
 
-  def engines_warning(mesg, *objects)
+  def engines_warning(mesg, *objs)
     EnginesRegistryError.new(mesg, :warning, *objs)
   end
 
