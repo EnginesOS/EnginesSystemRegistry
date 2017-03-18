2550fac1aaf98c9bab20d338de50bd64f695b8a3
James L Vodanovich <james@jvodan.com>
2017-03-18 12:02:46 +1100

    debug locked reg

--- src/registry/system_registry/file_locking.rb
+++ src/registry/system_registry/file_locking.rb
@@ -4,12 +4,16 @@ end
 
 def lock_tree
   if File.exist?(registry_lock)
-    sleep 0.5
-    sleep 0.6 if File.exist?(registry_lock)
     log_error_mesg("REGISTRY_LOCKED")
-    sleep 0.5 if File.exist?(registry_lock)
-    log_error_mesg("Failed to lock Retrying",registry_lock) if File.exist?(registry_lock) if File.exist?(registry_lock)
     sleep 0.5
+    log_error_mesg("REGISTRY_LOCKED .5" )
+    sleep 0.6 if File.exist?(registry_lock)
+    log_error_mesg("REGISTRY_LOCKED 1.1")
+    sleep 0.5 if File.exist?(registry_lock)
+    log_error_mesg("REGISTRY_LOCKED 1.6")
+    log_error_mesg("Failed to lock Retrying",registry_lock) if File.exist?(registry_lock) if File.exist?(registry_lock)   
+    sleep 0.5
+    log_error_mesg("REGISTRY_LOCKED 2.1")
     return log_error_mesg("Failed to lock",registry_lock) if File.exist?(registry_lock)
   end
   FileUtils.touch(registry_lock)
