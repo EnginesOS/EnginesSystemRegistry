c404e64eab29feedf69b628dc201e3600f325828
James L Vodanovich <james@jvodan.com>
2017-05-20 13:24:18 +1000

    dbg

--- src/registry/sub_registeries/services_registry.rb
+++ src/registry/sub_registeries/services_registry.rb
@@ -1,8 +1,8 @@
 class ServicesRegistry < SubRegistry
   # @Boolean returns true | false if servcice hash is registered in service tree
   def service_is_registered?(query_params)
-
-    is_ns_tp_node_registered?(@registry, query_params,[:parent_engine, :service_handle] )
+    STDERR.puts(' QUERY PARAS ' + query_params.to_s + ' for keys  ' + [:parent_engine, :service_handle].to_s)
+    is_ns_tp_node_registered?(@registry, query_params, [:parent_engine, :service_handle] )
   end
 
   # Add The service_hash to the services registry branch
