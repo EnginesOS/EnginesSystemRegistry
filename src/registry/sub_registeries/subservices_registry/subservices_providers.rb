1811f1976a84178bc3db34c363dee996989dc136
james <james@engines.org>
2019-02-15 17:47:32 +1100

    dbg

--- src/registry/sub_registeries/subservices_registry/subservices_providers.rb
+++ src/registry/sub_registeries/subservices_registry/subservices_providers.rb
@@ -35,6 +35,7 @@ module SubservicesProviders
 
   def provider_node(params)
     STDERR.puts("\n subservices_providers node " + subservices_providers.to_s)
+    STDERR.puts("\n params  " + params.to_s)
     get_pns_type_path_node(subservices_providers, params)
   end
 
